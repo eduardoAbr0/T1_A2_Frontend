c0a41646304e3c708a4e97158f0951cb803771da
ED A <eduardo.br@tecjerez.edu.mx>
2026-02-27 20:10:53 -0600

    Tabla para alumnos agregados

---
+++ src/components/ListadoAlumnos.vue
@@ -0,0 +1,54 @@
+<script setup>
+const props = defineProps({
+    alumnos: Array
+});
+</script>
+
+<template>
+    <div class="card">
+        <h5>Alumnos Registrados</h5>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover align-middle">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Control</th>
+                            <th>Nombre</th>
+                            <th>Edad</th>
+                            <th>Semestre</th>
+                            <th>Materias</th>
+                            <th>Especialidad</th>
+                            <th>Extraescolares</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-if="alumnos.length === 0">
+                            <td class="text-center">No hay alumnos registrados.</td>
+                        </tr>
+                        <tr v-for="(al, index) in alumnos" :key="index">
+                            <td><span class="fw-bold">{{ al.numControl }}</span></td>
+                            <td>{{ al.nombreCompleto }}</td>
+                            <td>{{ al.edad }} años</td>
+                            <td class="text-center">{{ al.semestre }}</td>
+                            <td>
+                                <span v-for="m in al.materiasAgregadas" :key="m">
+                                    | {{ m }} |
+                                </span>
+                            </td>
+                            <td>
+                                <span v-for="e in al.materiasEspecialidad" :key="e">
+                                    | {{ e }} |
+                                </span>
+                            </td>
+                            <td>
+                                <span v-for="act in al.actividadesExtraescolares" :key="act">
+                                    | {{ act }} |
+                                </span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import router from './router';
 
-const app = createApp(App).mount('#app');
+const app = createApp(App);
+app.use(router);
 app.use(PrimeVue);
+
+app.mount('#app');
